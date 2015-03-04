03779a796ad3644bd111434773f765d262d5c59c
Adam Heins <mail@adamheins.com>
2015-03-03 18:43:47 -0800

    Fixed bug in slate.py and added --force option.

--- slate.py
+++ slate.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import argparse
+import os.path
+import re
 
 # Defaults.
 OUT_FILE = 'seed.txt'
@@ -8,11 +10,11 @@ ROWS = 20
 COLS = 20
 
 
-def clean():
+def clean(out_file, rows, cols):
     """ Rewrites the file as a clean slate. """
-    with open(args.out, 'w') as out_file:
-        for row in range(args.rows):
-            for col in range(args.cols):
+    with open(out_file, 'w') as out_file:
+        for row in range(rows):
+            for col in range(cols):
                 out_file.write(FILL_CHAR)
             out_file.write('\n')
 
@@ -24,6 +26,8 @@ def main():
                         type=int, dest='rows', default=ROWS)
     parser.add_argument('-c', '--cols', help='Number of columns to fill.',
                         type=int, dest='cols', default=COLS)
+    parser.add_argument('-f', '--force', help="Don't ask before overwriting a "
+                        "file.", dest='force', action='store_true')
     args = parser.parse_args()
 
     # Error checking on arguments.
@@ -32,7 +36,15 @@ def main():
     if args.cols < 0:
         raise ValueError('--cols must not be negative.')
 
-    clean()
+    # Prompt user to confirm file overwrite, if force flag is not present.
+    edit = True
+    if not args.force and os.path.exists(args.out):
+        input_ = raw_input('The file {} already exists. Do you want to '
+                           'overwrite it? [Y/n] '.format(args.out))
+        edit = input_ == '' or re.match('[Yy](es)?', input_)
+
+    if edit:
+        clean(args.out, args.rows, args.cols)
 
 if __name__ == '__main__':
     main()
